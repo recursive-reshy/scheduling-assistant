581bd50e1132793416f4818a50819cd7908a42b7
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-11-10 20:59:24 +0800

    database.ts: Added config file

---
+++ src/config/database.ts
@@ -0,0 +1,22 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASE_URL!
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!
+
+// TODO: Add database type
+const supabase = createClient( supabaseUrl, supabaseAnonKey )
+
+export default supabase
+
+export const testDatabaseConnection = async () => {
+  try {
+    const { data, error } = await supabase.from( 'test' ).select( '*' )
+
+    if ( error ) throw error
+    
+    return data
+  } catch ( error ) {
+    console.error( `Error testing database connection: ${JSON.stringify( error )}` )
+    throw error
+  }
+}
